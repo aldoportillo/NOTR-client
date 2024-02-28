7742b60bd57e810d552ef6603f3c8fce1a1060f5
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-02-28 13:55:00 -0600

    Add analytics and speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import './index.css';
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -21,6 +23,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           draggable
           pauseOnHover={false}
           theme="dark" />
+      <Analytics />
+      <SpeedInsights />
     </Router>
   </React.StrictMode>,
 )
