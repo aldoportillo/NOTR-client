85e3c2b2a8ed40afccd10c71841b15e7ee861a2e
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-06-17 11:57:02 -0500

    Implement styles and isActiveState

--- src/components/BarcodeScanner.tsx
+++ src/components/BarcodeScanner.tsx
@@ -1,24 +1,27 @@
-import { useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import styled from 'styled-components';
 import { BrowserMultiFormatReader, NotFoundException } from '@zxing/library';
 
 const BarcodeScanner = () => {
   const videoRef = useRef(null);
+  const [isActive, setIsActive] = useState(false);
   const codeReader = new BrowserMultiFormatReader();
 
   useEffect(() => {
+    if (!isActive) return;
+
     const startScanner = async () => {
       try {
         const videoInputDevices = await codeReader.listVideoInputDevices();
         codeReader.decodeFromVideoDevice(videoInputDevices[0].deviceId, videoRef.current, (result, err) => {
           if (result) {
-            console.log(`Barcode read: ${result}`);
-            codeReader.reset();  // Optionally reset the scanner after a successful read
+            console.log(`Barcode read: ${result.getText()}`);
+            setIsActive(false);
+            codeReader.reset(); 
           }
 
           if (err) {
-            if (err instanceof NotFoundException) {
-              // Ignore NotFoundException, no barcode found
-            } else {
+            if (!(err instanceof NotFoundException)) {
               console.error(err);
             }
           }
@@ -33,14 +36,54 @@ const BarcodeScanner = () => {
     return () => {
       codeReader.reset();
     };
-  }, []);
+  }, [isActive]);
 
   return (
-    <div>
-      <video ref={videoRef} style={{ width: '100%' }} />
-      <p>Point the camera at a barcode.</p>
-    </div>
+    <Container>
+      {!isActive && (
+        <ScanButton onClick={() => setIsActive(true)}>Activate Scanner</ScanButton>
+      )}
+      {isActive && (
+        <>
+          <StyledVideo ref={videoRef} />
+          <InfoText>Point the camera at a barcode.</InfoText>
+        </>
+      )}
+    </Container>
   );
 };
 
 export default BarcodeScanner;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: var(--background, #36393f);
+  color: white;
+  padding: 20px;
+  box-sizing: border-box;
+`;
+
+const StyledVideo = styled.video`
+  width: 100%;
+  border: 3px solid var(--accent, rgb(244, 154, 115));
+`;
+
+const InfoText = styled.p`
+  color: var(--overlay, #838383);
+`;
+
+const ScanButton = styled.button`
+  background-color: var(--accent, rgb(244, 154, 115));
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  margin-top: 20px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: var(--header, #23272a);
+  }
+`;
