81414eb6f5595bbdbdf6107dbfb119b8f2045752
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-06-18 10:35:45 -0500

    Create Beverage Type

---
+++ src/types/Beverage.ts
@@ -0,0 +1,17 @@
+export interface Beverage {
+    id: number;
+    verified: boolean;
+    name: string;
+    abv: number;
+    ounces: number;
+    upc_code: string;
+    flavors?: string;
+    calories?: number;
+    ethanol?: number;
+    fat?: number;
+    carb?: number;
+    sugar?: number;
+    added_sugar?: number;
+    protein?: number;
+    creator_id?: string;
+}
