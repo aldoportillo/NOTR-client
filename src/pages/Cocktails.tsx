3e288ee4f640d567c8d5b5d9314e99b1af4e4540
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-06-24 16:07:57 -0500

    Add search bar for cocktails

--- src/pages/Cocktails.tsx
+++ src/pages/Cocktails.tsx
@@ -3,18 +3,24 @@ import LoadingGif from '../assets/loading.gif'
 import { CocktailData } from '../types/CocktailData'
 import styled from 'styled-components'
 import { Helmet } from 'react-helmet'
+import { useState } from 'react'
 
 interface CocktailProps {
     cocktailData: CocktailData[];
     loading: boolean;
 }
 
-export default function Cocktails({cocktailData, loading}: CocktailProps) {
+export default function Cocktails({ cocktailData, loading }: CocktailProps) {
 
+    const [searchTerm, setSearchTerm] = useState('');
+
+
+    const filteredCocktails = cocktailData.filter(cocktail =>
+        cocktail.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
     
-
-    const renderCocktails = cocktailData?.map(cocktail => {
-        const {id, name, glass, slug, image_url} = cocktail
+    const renderCocktails = filteredCocktails.map(cocktail => {
+        const {id, name, glass, slug, image_url} = cocktail;
         return (
             <CocktailCard key={id}>
                 <Link to={`/cocktail/${slug}`} state={{ from: "cocktail", data: cocktail}}>
@@ -22,28 +28,37 @@ export default function Cocktails({cocktailData, loading}: CocktailProps) {
                     <img src={image_url} alt={`${name} inside a ${glass} glass`} />
                 </Link>
             </CocktailCard>
-        )
-    })
-    
-  return (
-    <>
-        <Wrapper>
-            <h2>Cocktails</h2>
+        );
+    });
+
+    return (
+        <>
+            <Wrapper>
+                <h2>Cocktails</h2>
+                <Input
+                    type="text"
+                    placeholder="Search cocktails..."
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    style={{ margin: "20px", padding: "10px", width: "90%" }}
+                />
+
                 {loading ?
-                <img src={LoadingGif} className="loader" alt="loader"/> :
-                <CocktailListContainer>
-                    {renderCocktails}
-                </CocktailListContainer>}  
-        </Wrapper>
-        <Helmet>
-            <title>Cocktails | Neat on the Rocks</title>
-            <meta name="description" content="Welcome to Neat on the Rocks, your passport to a new realm of cocktail enjoyment that blends health-conscious choices with irresistible flavors. For bartenders and cocktail aficionados who value both taste and well-being, we present an innovative platform that transforms the way you drink." />
-            <meta name="keywords" content="alcohol, calories, ethanol, abv, nutrition, glassware, bar, bartender, vodka, gin, tequila, best tequila, instructions" />
-            <meta property="og:imageUrl" content="https://res.cloudinary.com/dkhtrg1ts/imageUrl/upload/v1702322801/NeatontheRocks/Cocktails/photo-1470337458703-46ad1756a187_xcfnzd.avif" />
-            <meta name="twitter:imageUrl" content="https://res.cloudinary.com/dkhtrg1ts/imageUrl/upload/v1702322801/NeatontheRocks/Cocktails/photo-1470337458703-46ad1756a187_xcfnzd.avif" />
-      </Helmet>
-    </>
-  )
+                    <img src={LoadingGif} className="loader" alt="loader" /> :
+
+                    <CocktailListContainer>
+                        {renderCocktails}
+                    </CocktailListContainer>}
+            </Wrapper>
+            <Helmet>
+                <title>Cocktails | Neat on the Rocks</title>
+                <meta name="description" content="Welcome to Neat on the Rocks, your passport to a new realm of cocktail enjoyment that blends health-conscious choices with irresistible flavors. For bartenders and cocktail aficionados who value both taste and well-being, we present an innovative platform that transforms the way you drink." />
+                <meta name="keywords" content="alcohol, calories, ethanol, abv, nutrition, glassware, bar, bartender, vodka, gin, tequila, best tequila, instructions" />
+                <meta property="og:imageUrl" content="https://res.cloudinary.com/dkhtrg1ts/imageUrl/upload/v1702322801/NeatontheRocks/Cocktails/photo-1470337458703-46ad1756a187_xcfnzd.avif" />
+                <meta name="twitter:imageUrl" content="https://res.cloudinary.com/dkhtrg1ts/imageUrl/upload/v1702322801/NeatontheRocks/Cocktails/photo-1470337458703-46ad1756a187_xcfnzd.avif" />
+            </Helmet>
+        </>
+    )
 }
 
 const Wrapper = styled.div`
@@ -51,6 +66,7 @@ const Wrapper = styled.div`
     align-self: center;
     flex-direction: column;
     align-items: center;
+    align-self: flex-start;
 `
 
 const CocktailListContainer = styled.div`
@@ -96,3 +112,18 @@ const CocktailCard = styled.a`
         }
       }
 `
+
+const Input = styled.input`
+  margin: 20px;
+  width: 100%;
+  font-size: 16px; 
+  border: 1px solid #ccc; 
+  border-radius: 5px;
+  box-shadow: 0 2px 5px rgba(0,0,0,0.1);
+
+  &:focus {
+    outline: none;
+    border-color: var(--accent); 
+    box-shadow: 0 0 5px rgba(0,123,255,0.5);
+  }
+`;
