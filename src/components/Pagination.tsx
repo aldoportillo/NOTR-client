0b4f6264f6040b523e5df8a238dd674b1c013472
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-06-25 12:12:12 -0500

    Update pagination to accept cocktails as children

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,12 +1,85 @@
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom'
+
+
+function Pagination({ children, totalItems, searchTerm }) {
+    const [pageNumbers, setPageNumbers] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 9;
+
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentCocktails = children.slice(indexOfFirstItem, indexOfLastItem);
+
+
+    const renderCocktails = currentCocktails.map(cocktail => {
+        const {id, name, glass, slug, image_url} = cocktail;
+        return (
+            <CocktailCard key={id}>
+                <Link to={`/cocktail/${slug}`} state={{ from: "cocktail", data: cocktail}}>
+                    <h3>{name}</h3>
+                    <img src={image_url} alt={`${name} inside a ${glass} glass`} />
+                </Link>
+            </CocktailCard>
+        );
+    });
+
+    useEffect(() => {
+        const totalPages = Math.ceil(totalItems / itemsPerPage);
+        const numbers = [];
+        for (let i = 1; i <= totalPages; i++) {
+            numbers.push(i);
+        }
+        setPageNumbers(numbers);
+    }, [totalItems, itemsPerPage]);
+
+
+    
+
+    useEffect(() => {
+        setCurrentPage(1);
+    }, [searchTerm]);
+
+
+    return (
+        <PaginationWrapper>
+            <CocktailListContainer>
+                {renderCocktails}
+            </CocktailListContainer>
+            <PaginationFooter>
+                {pageNumbers.map(number => (
+                    <PageNumber
+                        key={number}
+                        whileHover={{ scale: 1.1 }}
+                        whileTap={{ scale: 0.9 }}
+                        className={currentPage === number ? 'active' : ''}
+                        onClick={() => setCurrentPage(number)}
+                    >
+                        {number}
+                    </PageNumber>
+                ))}
+            </PaginationFooter>
+        </PaginationWrapper>
+    );
+}
+
+export default Pagination;
+
 
 const PaginationWrapper = styled.div`
     display: flex;
+    flex-direction: column;
     justify-content: center;
     gap: 10px;
     margin-top: 20px;
+`;
+
+const PaginationFooter = styled.div`
+    display: flex;
+    justify-content: center;
+    gap: 10px;
 `;
 
 const PageNumber = styled(motion.button)`
@@ -26,33 +99,45 @@ const PageNumber = styled(motion.button)`
     }
 `;
 
-function Pagination({  totalItems, itemsPerPage, currentPage, onPageChange }) {
-    const [pageNumbers, setPageNumbers] = useState([]);
+const CocktailListContainer = styled.div`
 
-    useEffect(() => {
-        const totalPages = Math.ceil(totalItems / itemsPerPage);
-        const numbers = [];
-        for (let i = 1; i <= totalPages; i++) {
-            numbers.push(i);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 15px;
+
+    @media only screen and (min-width:1025px) {
+      flex-direction: row;
+      flex-wrap: wrap;
+    }
+`
+
+const CocktailCard = styled.a`
+    a {
+        width: 80vw;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        background-color: #838383;
+        border-radius: 1vh;
+        margin: 5px;
+        
+    }
+  
+    a > img{
+        width: 100%;
+        border-radius: 1vh;
+    }
+
+    @media only screen and (min-width:1025px) {
+        a{
+          width: 25vw;
         }
-        setPageNumbers(numbers);
-    }, [totalItems, itemsPerPage]);
-
-    return (
-        <PaginationWrapper>
-            {pageNumbers.map(number => (
-                <PageNumber
-                    key={number}
-                    whileHover={{ scale: 1.1 }}
-                    whileTap={{ scale: 0.9 }}
-                    className={currentPage === number ? 'active' : ''}
-                    onClick={() => onPageChange(number)}
-                >
-                    {number}
-                </PageNumber>
-            ))}
-        </PaginationWrapper>
-    );
-}
-
-export default Pagination;
+        
+        a >img{
+          width: 25vw;
+          height: 18vw;
+        }
+      }
+`
--- src/pages/Cocktails.tsx
+++ src/pages/Cocktails.tsx
@@ -1,9 +1,8 @@
-import { Link } from 'react-router-dom'
 import LoadingGif from '../assets/loading.gif'
 import { CocktailData } from '../types/CocktailData'
 import styled from 'styled-components'
 import { Helmet } from 'react-helmet'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import Pagination from '../components/Pagination'
 
 interface CocktailProps {
@@ -13,54 +12,32 @@ interface CocktailProps {
 
 export default function Cocktails({ cocktailData, loading }: CocktailProps) {
     const [searchTerm, setSearchTerm] = useState('');
-    const [currentPage, setCurrentPage] = useState(1);
-    const itemsPerPage = 9;
-
-    useEffect(() => {
-        setCurrentPage(1);
-    }, [searchTerm]);
-
+    
     const filteredCocktails = cocktailData.filter(cocktail =>
         cocktail.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
-    const indexOfLastItem = currentPage * itemsPerPage;
-    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-    const currentCocktails = filteredCocktails.slice(indexOfFirstItem, indexOfLastItem);
-
-    const renderCocktails = currentCocktails.map(cocktail => {
-        const {id, name, glass, slug, image_url} = cocktail;
-        return (
-            <CocktailCard key={id}>
-                <Link to={`/cocktail/${slug}`} state={{ from: "cocktail", data: cocktail}}>
-                    <h3>{name}</h3>
-                    <img src={image_url} alt={`${name} inside a ${glass} glass`} />
-                </Link>
-            </CocktailCard>
-        );
-    });
-
     return (
         <>
             <Wrapper>
-                <h2>Cocktails</h2>
-                <Input
-                    type="text"
-                    placeholder="Search cocktails..."
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                />
+                <Header>
+                    <h2>Cocktails</h2>
+                    <Input
+                        type="text"
+                        placeholder="Search cocktails..."
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                    />
+                    <button>Toggle Fridge</button>
+                    <button>Toggle Recommendations</button>
+                </Header> 
                 {loading ? <img src={LoadingGif} className="loader" alt="loader" /> : (
                     <>
-                        <CocktailListContainer>
-                            {renderCocktails}
-                        </CocktailListContainer>
+                        
                         <Pagination
                             totalItems={filteredCocktails.length}
-                            itemsPerPage={itemsPerPage}
-                            currentPage={currentPage}
-                            onPageChange={setCurrentPage}
-                        />
+                            searchTerm={searchTerm}
+                        >{filteredCocktails}</Pagination>
                     </>
                 )}
             </Wrapper>
@@ -83,49 +60,10 @@ const Wrapper = styled.div`
     align-self: flex-start;
 `
 
-const CocktailListContainer = styled.div`
-
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    gap: 15px;
-
-    @media only screen and (min-width:1025px) {
-      flex-direction: row;
-      flex-wrap: wrap;
-    }
-`
 
 
-const CocktailCard = styled.a`
-    a {
-        width: 80vw;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        background-color: #838383;
-        border-radius: 1vh;
-        margin: 5px;
-        
-    }
-  
-    a > img{
-        width: 100%;
-        border-radius: 1vh;
-    }
 
-    @media only screen and (min-width:1025px) {
-        a{
-          width: 25vw;
-        }
-        
-        a >img{
-          width: 25vw;
-          height: 18vw;
-        }
-      }
-`
+
 
 const Input = styled.input`
   margin: 20px;
@@ -141,3 +79,11 @@ const Input = styled.input`
     box-shadow: 0 0 5px rgba(0,123,255,0.5);
   }
 `;
+
+const Header = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+    margin: 20px;
+    `
